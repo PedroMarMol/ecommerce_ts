a1423d82b78ad2adca40a32fa455f60443185774
PedroMarMol <martosmoleropedro@gmail.com>
2023-05-17 11:45:46 +0200

    Implement render list function

--- src/app/components/List.tsx
+++ src/app/components/List.tsx
@@ -10,19 +10,21 @@ interface Props {
 }
 
 const List = ({users}: Props) => {
+  const renderList = (): JSX.Element[] => {
+    return users.map(user => {
+      return (
+        <li key={user.nick}>
+          <Image src={user.avatar} alt={`Avatar for ${user}`} width={150} height={150} />
+          <h4>{user.nick} (<small>{user.streakMonths}</small>)</h4>
+          <p>{user.description?.substring(0, 100)}</p>
+        </li>
+      )
+    })
+  }
+
   return (
     <ul>
-      {
-        users.map(user => {
-          return (
-            <li key={user.nick}>
-              <Image src={user.avatar} alt={`Avatar for ${user}`} width={150} height={150} />
-              <h4>{user.nick} (<small>{user.streakMonths}</small>)</h4>
-              <p>{user.description?.substring(0, 100)}</p>
-            </li>
-          )
-        })
-      }
+      {renderList()}
     </ul>
   )
 }
